522bd13e857c328cc13ba6e6704dbcb5f38f54ec
pooja_chavan04 <pooja.chavan@nitorinfotech.com>
2023-03-20 16:10:42 +0530

    google map integration

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,169 @@
-import { Component } from '@angular/core';
+import { Component, AfterViewInit, ViewChild, ElementRef } from "@angular/core";
+import{MarkerClusterer} from "@googlemaps/markerclusterer"
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"]
 })
-export class AppComponent {
-  title = 'GoogleMap';
+export class AppComponent implements AfterViewInit {
+  @ViewChild("mapContainer") gmap: ElementRef;
+  map: google.maps.Map;
+  lat = -34.397;
+  lng = 150.644;
+
+   locations = [
+    { lat: -31.56391, lng: 147.154312 },
+    { lat: -33.718234, lng: 150.363181 },
+    { lat: -33.727111, lng: 150.371124 },
+    { lat: -33.848588, lng: 151.209834 },
+    { lat: -33.851702, lng: 151.216968 },
+    { lat: -34.671264, lng: 150.863657 },
+    { lat: -35.304724, lng: 148.662905 },
+    { lat: -36.817685, lng: 175.699196 },
+    { lat: -36.828611, lng: 175.790222 },
+    { lat: -37.75, lng: 145.116667 },
+    { lat: -37.759859, lng: 145.128708 },
+    { lat: -37.765015, lng: 145.133858 },
+    { lat: -37.770104, lng: 145.143299 },
+    { lat: -37.7737, lng: 145.145187 },
+    { lat: -37.774785, lng: 145.137978 },
+    { lat: -37.819616, lng: 144.968119 },
+    { lat: -38.330766, lng: 144.695692 },
+    { lat: -39.927193, lng: 175.053218 },
+    { lat: -41.330162, lng: 174.865694 },
+    { lat: -42.734358, lng: 147.439506 },
+    { lat: -42.734358, lng: 147.501315 },
+    { lat: -42.735258, lng: 147.438 },
+    { lat: -43.999792, lng: 170.463352 },
+  ];
+
+  markers = [
+    {
+      position: new google.maps.LatLng(40.73061, 73.935242),
+      map: this.map,
+      title: "Marker 1"
+    },
+    {
+      position: new google.maps.LatLng(32.06485, 34.763226),
+      map: this.map,
+      title: "Marker 2"
+    }
+  ];
+ 
+  //Coordinates to set the center of the map
+  coordinates = new google.maps.LatLng(this.lat, this.lng);
+
+  mapOptions: google.maps.MapOptions = {
+    center: this.coordinates,
+    zoom: 8,
+    
+  };
+
+  //Default Marker
+  marker = new google.maps.Marker({
+    position: this.coordinates,
+    map: this.map,
+    title: "Marker 1"
+  });
+
+  polygonPath: [
+    { lat: 25.774, lng: -80.19 },
+    { lat: 18.466, lng: -66.118 },
+    { lat: 32.321, lng: -64.757 },
+    { lat: 25.774, lng: -80.19 },
+  ];
+  
+
+  ngAfterViewInit(): void {
+    this.mapInitializer();
+    this.addPolygon();
+    this.addClusters();
+  }
+  
+
+  mapInitializer(): void {
+    this.map = new google.maps.Map(this.gmap.nativeElement, this.mapOptions);
+    //Adding Click event to default marker
+    this.marker.addListener("click", () => {
+      const infoWindow = new google.maps.InfoWindow({
+        content: this.marker.getTitle()
+      });
+      infoWindow.open(this.marker.getMap(), this.marker);
+    });
+  }
+
+  addMarkers(): void {
+    this.markers.forEach(markerInfo => {
+      //Creating a new marker object
+      const marker = new google.maps.Marker({
+        ...markerInfo
+      });
+
+      //creating a new info window with markers info
+      const infoWindow = new google.maps.InfoWindow({
+        content: marker.getTitle()
+      });
+
+      //Add click event to open info window on marker
+      marker.addListener("click", () => {
+        infoWindow.open(marker.getMap(), marker);
+      });
+
+      //Adding marker to google map
+      marker.setMap(this.map);
+    });
+  }
+
+  addPolygon() {
+    const coordinates = [
+      { lat: 25.774, lng: -80.19 },
+      { lat: 18.466, lng: -66.118 },
+      { lat: 32.321, lng: -64.757 },
+      { lat: 25.774, lng: -80.19 },
+    ];
+    // Construct the polygon.
+    const polygon = new google.maps.Polygon({
+      paths: coordinates,
+      strokeColor: "#FF0000",
+      strokeOpacity: 0.8,
+      strokeWeight: 2,
+      fillColor: "#FF0000",
+      fillOpacity: 0.35,
+      editable: true,
+      draggable: true,
+    });
+
+    polygon.setMap(this.map);
+  }
+
+  addClusters() {
+    const infoWindow = new google.maps.InfoWindow({
+      content: "",
+      disableAutoPan: true,
+    });
+    const labels = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
+    // Add some markers to the map.
+    const markers = this.locations.map((position, i) => {
+      const label = labels[i % labels.length];
+      const marker = new google.maps.Marker({
+        position,
+        label,
+      });
+
+      // markers can only be keyboard focusable when they have click listeners
+      // open info window when marker is clicked
+      marker.addListener("click", () => {
+        infoWindow.setContent(label);
+        infoWindow.open(this.map, marker);
+      });
+
+      return marker;
+    });
+
+    // Add a marker clusterer to manage the markers.
+    new MarkerClusterer({ markers, map: this.map });
+  }
+ 
 }
